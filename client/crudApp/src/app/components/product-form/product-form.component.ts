16b737d88b8b327c678aefc1bd26d34b571cb7df
Nathandiv <k.n.hlungwani@gmail.com>
2025-01-27 11:30:15 +0200

    added imports

--- client/crudApp/src/app/components/product-form/product-form.component.ts
+++ client/crudApp/src/app/components/product-form/product-form.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule,RouterLink],
   templateUrl: './product-form.component.html',
   styleUrl: './product-form.component.css'
 })

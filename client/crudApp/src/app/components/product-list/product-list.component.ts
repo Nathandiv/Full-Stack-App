72ee95f1749c3a922fc97c1a98f2f34fdb0a0a7f
Nathandiv <k.n.hlungwani@gmail.com>
2025-01-27 11:27:00 +0200

    added profuctlist logic part

--- client/crudApp/src/app/components/product-list/product-list.component.ts
+++ client/crudApp/src/app/components/product-list/product-list.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../model/product';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,RouterModule],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+
+
+  products: Product[] = [];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.loadProducts();
+  }
+
+  loadProducts(): void {
+    this.productService.getAll().subscribe({
+      next: (data) => {
+        this.products = data;
+      },
+      error: (e) => console.error(e),
+    });
+  }
+
+  deleteProduct(id: number): void {
+    if (confirm('Are you sure you want to delete this product?')) {
+      this.productService.delete(id).subscribe({
+        next: () => {
+          this.loadProducts();
+        },
+        error: (e) => console.error(e),
+      });
+    }
+  }
 
 }

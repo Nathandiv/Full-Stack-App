f6b2c880ca207eed31fcb4d4bc751cb5c04cd757
Nathandiv <k.n.hlungwani@gmail.com>
2025-01-27 11:33:38 +0200

    added logic for product form

--- client/crudApp/src/app/components/product-form/product-form.component.ts
+++ client/crudApp/src/app/components/product-form/product-form.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../model/product';
 
 @Component({
   selector: 'app-product-form',
@@ -10,6 +12,70 @@ import { RouterLink } from '@angular/router';
   templateUrl: './product-form.component.html',
   styleUrl: './product-form.component.css'
 })
-export class ProductFormComponent {
+export class ProductFormComponent implements OnInit {
+
+  product: Product = {
+    id: 0,
+    name: '',
+    description: '',
+    price: 0,
+    image: ''
+  };
+  isEditMode = false;
+
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.params['id'];
+    if (id) {
+      this.isEditMode = true;
+      this.productService.get(id).subscribe({
+        next: (data) => {
+          this.product = data;
+        },
+        error: (e) => console.error(e)
+      });
+    }
+  }
+
+  
+  onFileSelected(event: any): void {
+    const file = event.target.files[0];
+    if (file) {
+    
+      if (file.size > 1024 * 1024) {
+        alert('File size should not exceed 1MB');
+        return;
+      }
+
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.product.image = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  onSubmit(): void {
+    if (this.isEditMode) {
+      this.productService.update(this.product.id!, this.product).subscribe({
+        next: () => {
+          this.router.navigate(['/products']);
+        },
+        error: (e) => console.error(e),
+      });
+    } else {
+      this.productService.create(this.product).subscribe({
+        next: () => {
+          this.router.navigate(['/products']);
+        },
+        error: (e) => console.error(e),
+      });
+    }
+  }
 
 }
